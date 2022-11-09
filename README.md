d7ee14fcad8d00df4d89f3cc8cf4a987afa9eae3
Silas <silas@insightengines.com>
2022-11-09 11:31:21 -0800

    update README run doc, add data dir to gitignore, minor refactor mainpy

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # BEES Data Engineering test
 
-docker-compose build
+## Run doc:
+
+### make a .env file with:<br> 
+MARVEL_PUBLIC_KEY="asdfghjkl1234567890"<br>
+MARVEL_PRIVATE_KEY"qwertyuiop0987654321"<br>
+
+### build the containers
+docker-compose build<br>
+
+### start the containers
 docker-compose up -d
 
-## example syntax, spark job can be run from master or worker after connecting to it via docker exec -it
+### connecting to spark container (master or worker(s))
 docker exec -it bees_spark-master_1 bash
+
+### execute task
 /opt/spark/bin/spark-submit --master spark://spark-master:7077 --driver-memory 1G --executor-memory 1G /opt/spark-apps/main.py
--- apps/main.py
+++ apps/main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import array_contains, explode, datediff, year, expr, to_date, collect_set, countDistinct, desc
+from pyspark.sql.functions import array_contains, explode, datediff, year, expr, to_date\
+    , collect_set, countDistinct, desc
 
 import requests
 import hashlib
@@ -19,7 +20,8 @@ def fetch_marvel(endpoint, save_dir):
     total = 1
 
     while offset < total:
-        full_url = 'https://gateway.marvel.com/v1/public/{0}?apikey={1}&hash={2}&ts={3}&limit={4}&offset={5}'.format(endpoint, PUBLIC_KEY, hashed, ts, PAGE_LIMIT, offset)
+        full_url = 'https://gateway.marvel.com/v1/public/{0}?apikey={1}&hash={2}&ts={3}&limit={4}&offset={5}'\
+            .format(endpoint, PUBLIC_KEY, hashed, ts, PAGE_LIMIT, offset)
         res = requests.get(full_url).json()
         file_write_time = int(time.time())
         ts_dir_name = '{0}/case/landing/{1}/uploaded_at={2}/'.format(save_dir, endpoint, file_write_time)
@@ -48,7 +50,8 @@ def silver_layer(spark_session, endpoint, save_dir):
             results = json.load(f).get("data").get("results")
 
             if endpoint == "characters":
-                columns = ["id", "name", "description", "comics_count", "events_count", "stories_count", "series_count", "date_of_upload"]
+                columns = ["id", "name", "description", "comics_count", "events_count", "stories_count",\
+                    "series_count", "date_of_upload"]
                 data = []
                 for result in results:
                     data.append((
@@ -85,37 +88,42 @@ def silver_layer(spark_session, endpoint, save_dir):
     
     print("Silver layer processing complete...")
 
-def gold_layer(spark_session):
+def gold_layer(spark_session, save_dir):
     print("Gold layer processing start...")
-    # gold_dir_name = '{0}/case/gold/{1}/'.format(save_dir, endpoint)
-    # gold_file_name = '{0}/case/gold/{1}/{2}.parquet'.format(save_dir, endpoint, endpoint)
-    # os.makedirs(gold_dir_name, exist_ok=True)
+    gold_dir_name = '{0}/case/gold/characters'.format(save_dir)
+    char_participation = '{0}/case/gold/characters/characters_participation.parquet'.format(save_dir)
+    char_count_each_year = '{0}/case/gold/characters/characters_count_each_year.parquet'.format(save_dir)
+    total_years_top10_chars = '{0}/case/gold/characters/total_years_top10_appeared.parquet'.format(save_dir)
+
+    os.makedirs(gold_dir_name, exist_ok=True)
 
     ## first one -- total days in events for each of the top 10 most appearing characters
     ep = spark_session.read.parquet("/opt/spark-data/case/silver/events/events.parquet")
     ep.createOrReplaceTempView("ep")
-    event_df = ep.select(explode(ep.list_of_characters).alias("character_name"),\
-        datediff(ep.end, ep.start).alias("datediff")).groupBy("character_name")\
-        .sum("datediff")
+    event_df = ep.select(explode(ep.list_of_characters).alias("character_name"), datediff(ep.end, ep.start)\
+        .alias("datediff")).groupBy("character_name").sum("datediff")
 
     cp = spark_session.read.parquet("/opt/spark-data/case/silver/characters/characters.parquet")
     cp.createOrReplaceTempView("cp")
     cp.select(cp.id, cp.name, cp.comics_count, cp.series_count, cp.stories_count, cp.events_count)\
         .join(event_df, cp.name == event_df.character_name).orderBy(cp.comics_count, ascending=False).limit(10).show()
+    cp.write.mode('overwrite').parquet(char_participation)
 
 
-    ##second one -- the number of characters appearing each year in events
+    ## second one -- the number of characters appearing each year in events
     eventParquet = spark_session.read.parquet("/opt/spark-data/case/silver/events/events.parquet")
     eventParquet.createOrReplaceTempView("eventParquet")
     eventParquet.select(explode(eventParquet.list_of_characters).alias("character_name"),\
         eventParquet.id, eventParquet.title, eventParquet.description, to_date(eventParquet.start)\
         .alias("start_year"), to_date(eventParquet.end).alias("end_year"))\
         .withColumn('generanged_date', explode(expr('sequence(start_year,  end_year, interval 1 year)')))\
-        .groupBy(year("generanged_date").alias("generanged_year")).agg(countDistinct("character_name").alias("character_count")).orderBy("generanged_year").show(n=1000)
-        
-    # replace countDistinct with collect_set to see the set of character names
-    # .groupBy(year("generanged_date").alias("generanged_year")).agg(collect_set("character_name").alias("character_set")).orderBy("generanged_year").show(n=1000)
-
+        .groupBy(year("generanged_date").alias("generanged_year")).agg(countDistinct("character_name")\
+            .alias("character_count")).orderBy("generanged_year").show(n=100)
+    eventParquet.write.mode('overwrite').parquet(char_count_each_year)
+    
+    # replace countDistinct with collect_set for character names (replace line 120+121 with line 125-126 below)
+    # .groupBy(year("generanged_date").alias("generanged_year")).agg(collect_set("character_name")\
+    #       .alias("character_set")).orderBy("generanged_year").show(n=1000)
 
 
     ## third one -- the total number of years each of the top 10 most appearing characters appeared in events
@@ -128,29 +136,29 @@ def gold_layer(spark_session):
     epff = epdf.select(explode(epdf.list_of_characters).alias("character_name"), epdf.id, epdf.title, epdf.description, to_date(epdf.start)\
         .alias("start_year"), to_date(epdf.end).alias("end_year"))\
         .withColumn('generanged_date', explode(expr('sequence(start_year,  end_year, interval 1 year)')))
-    epff.join(char_p, 'character_name', 'inner').groupBy("character_name").count().show(n=1000)
-
-
+    epff.join(char_p, 'character_name', 'inner').groupBy("character_name").count().show()
+    epff.write.mode('overwrite').parquet(total_years_top10_chars)
     print("Gold layer processing end...")
 
 
 
 
 def main():
-    print("spark job begin...")
+    print("Spark job begin...")
     sp_sess = SparkSession.builder.appName("marvel-bodyguard").getOrCreate()
 
-    # fetch_marvel("characters", "/opt/spark-data")
-    # fetch_marvel("events", "/opt/spark-data")
+    # landing code
+    fetch_marvel("characters", "/opt/spark-data")
+    fetch_marvel("events", "/opt/spark-data")
 
-    #silver code -- doesnt have to happen serially
-    # silver_layer(sp_sess, "characters", "/opt/spark-data")
-    # silver_layer(sp_sess, "events", "/opt/spark-data")
+    # silver code
+    silver_layer(sp_sess, "characters", "/opt/spark-data")
+    silver_layer(sp_sess, "events", "/opt/spark-data")
 
-    #gold layer
-    gold_layer(sp_sess)
+    # gold layer
+    gold_layer(sp_sess, "/opt/spark-data")
 
-    print("spark job completed...")
+    print("Spark job completed...")
   
 if __name__ == '__main__':
   main()
